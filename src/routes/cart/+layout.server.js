cefbab89c18c161f5619e8d09d355cc4ef704ad6
Gurparkash-Singh <singhgk.fateh@gmail.com>
2025-05-09 15:13:38 -0400

    Fixed Invalid status code Error

--- src/routes/cart/+layout.server.js
+++ src/routes/cart/+layout.server.js
@@ -3,6 +3,6 @@ import { redirect } from "@sveltejs/kit";
 
 export async function load({locals}) {
     if (!locals.admin) {
-        throw redirect("/", 302);
+        throw redirect(302, "/profile");
     }
 }
